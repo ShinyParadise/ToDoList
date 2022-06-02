7f299280548313b872387c95f7e8517f07d2f953
ShinyParadise <shinodise@gmail.com>
2022-06-02 10:11:41 +0300

    removed unused viewmodels

--- app/src/main/java/com/example/todolist/ui/detailscreen/DetailedListViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.todolist.ui.detailscreen;
-
-import androidx.lifecycle.ViewModel;
-
-public class DetailedListViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/todolist/ui/listscreen/ListsViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.todolist.ui.listscreen;
-
-import androidx.lifecycle.ViewModel;
-
-public class ListsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
