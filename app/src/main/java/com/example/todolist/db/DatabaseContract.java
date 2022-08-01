61e484182c498dad0197534b7683b5d366e61818
ShinyParadise <shinodise@gmail.com>
2022-08-01 15:22:55 +0300

    formatting fix

--- app/src/main/java/com/example/todolist/db/DatabaseContract.java
+++ app/src/main/java/com/example/todolist/db/DatabaseContract.java
@@ -26,18 +26,32 @@ public final class DatabaseContract {
 
         public static final String SELECT_ALL_LISTS = String.format(
                 "SELECT %s, %s, %s FROM %s",
-                _ID, COLUMN_HEADER, COLUMN_DESCRIPTION, TABLE_NAME
+                _ID,
+                COLUMN_HEADER,
+                COLUMN_DESCRIPTION,
+                TABLE_NAME
         );
 
-        public static final String SELECT_LIST_BY_ID = "SELECT * FROM " + TABLE_NAME
-                + " WHERE " + _ID + " = (?)";
-        public static final String SELECT_LIST_BY_HEADER = "SELECT * FROM " + TABLE_NAME
-                + " WHERE " + COLUMN_HEADER + " = (?)";
+        public static final String SELECT_LIST_BY_ID = String.format(
+                "SELECT * FROM %s WHERE %s = (?);",
+                TABLE_NAME,
+                _ID
+        );
 
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
 
-        public static final String INSERT_VALUES = "INSERT INTO " + TABLE_NAME + " ("
-                + COLUMN_HEADER + ", " + COLUMN_DESCRIPTION + ") " + "VALUES (?, ?)";
+        public static final String INSERT_VALUES = String.format(
+                "INSERT INTO %s (%s, %s) VALUES (?, ?)",
+                TABLE_NAME,
+                COLUMN_HEADER,
+                COLUMN_DESCRIPTION
+        );
+
+        public static final String SELECT_LAST_ITEM = String.format(
+                "SELECT * FROM %s ORDER BY %s DESC LIMIT 1",
+                TABLE_NAME,
+                _ID
+        );
     }
 
     public static abstract class ListItemTable implements BaseColumns {
@@ -65,26 +79,40 @@ public final class DatabaseContract {
 
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
 
-        public static final String INSERT_VALUES = "INSERT INTO " + TABLE_NAME + " ("
-                + COLUMN_DESCRIPTION + ", " + COLUMN_FK_LIST + ") " + "VALUES (?, ?)";
+        public static final String INSERT_VALUES = String.format(
+                "INSERT INTO %s (%s, %s) VALUES (?, ?);",
+                TABLE_NAME,
+                COLUMN_DESCRIPTION,
+                COLUMN_FK_LIST
+        );
 
-        public static final String UPDATE_LIST_ITEM = "UPDATE " + TABLE_NAME
-                + " SET " + COLUMN_DESCRIPTION + " = (?), "
-                + COLUMN_IS_CHECKED + " = (?) WHERE " + _ID + " = (?);";
+        public static final String UPDATE_LIST_ITEM = String.format(
+                "UPDATE %s SET %s = (?), %s = (?) WHERE %s = (?)",
+                TABLE_NAME,
+                COLUMN_DESCRIPTION,
+                COLUMN_IS_CHECKED,
+                _ID
+        );
 
-        public static final String SELECT_LIST_ITEM_BY_ID = "SELECT * FROM " + TABLE_NAME
-                + " WHERE " + _ID + " = (?);";
+        public static final String SELECT_LIST_ITEM_BY_ID = String.format(
+                "SELECT * FROM %s WHERE %s = (?)",
+                TABLE_NAME,
+                _ID
+        );
     }
 
-    public static String SELECT_SINGLE_LIST_ITEMS =
-            "SELECT " + ListItemTable.TABLE_NAME + "." + ListItemTable._ID
-            + ", " + ListItemTable.TABLE_NAME + "." + ListItemTable.COLUMN_DESCRIPTION
-            + ", " + ListItemTable.TABLE_NAME + "."+ ListItemTable.COLUMN_FK_LIST
-            + ", " + ListItemTable.TABLE_NAME + "."+ ListItemTable.COLUMN_IS_CHECKED
-            + " FROM " + ToDoListTable.TABLE_NAME + " INNER JOIN " + ListItemTable.TABLE_NAME
-            + " ON " + ToDoListTable.TABLE_NAME + "." + ToDoListTable._ID + " = "
-            + ListItemTable.TABLE_NAME + "." + ListItemTable.COLUMN_FK_LIST + " WHERE "
-            + ToDoListTable.TABLE_NAME + "." + ToDoListTable._ID + " = (?)";
+    public static String SELECT_SINGLE_LIST_ITEMS = String.format(
+            "SELECT %s.%s, %s.%s, %s.%s, %s.%s FROM %s INNER JOIN %s ON %s.%s = %s.%s WHERE %s.%s = (?);",
+            ListItemTable.TABLE_NAME, ListItemTable._ID,
+            ListItemTable.TABLE_NAME, ListItemTable.COLUMN_DESCRIPTION,
+            ListItemTable.TABLE_NAME, ListItemTable.COLUMN_FK_LIST,
+            ListItemTable.TABLE_NAME, ListItemTable.COLUMN_IS_CHECKED,
+            ToDoListTable.TABLE_NAME,
+            ListItemTable.TABLE_NAME,
+            ToDoListTable.TABLE_NAME, ToDoListTable._ID,
+            ListItemTable.TABLE_NAME, ListItemTable.COLUMN_FK_LIST,
+            ToDoListTable.TABLE_NAME, ToDoListTable._ID
+    );
 
     public static String NULL = "NULL";
 }
