222def18939f949021a1b1d0d75bd4f54101c0b7
ShinyParadise <shinodise@gmail.com>
2022-08-10 12:58:01 +0300

    fixed and simplified sorting algorithm

--- app/src/main/java/com/example/todolist/dto/ListItemComparator.java
+++ app/src/main/java/com/example/todolist/dto/ListItemComparator.java
@@ -8,13 +8,9 @@ public class ListItemComparator implements Comparator<ListItem> {
     public int compare(@NonNull ListItem firstItem, ListItem secondItem) {
         if (!firstItem.getState() && secondItem.getState())
             return -1;
-        if (firstItem.getState() == secondItem.getState() &&
-            firstItem.getUpdatedAt().isBefore(secondItem.getUpdatedAt())) {
-            return -1;
-        }
-        if (firstItem.getState() == secondItem.getState() &&
-            firstItem.getUpdatedAt().isAfter(secondItem.getUpdatedAt())) {
-            return 1;
+
+        if (firstItem.getState() == secondItem.getState()) {
+            return -firstItem.getUpdatedAt().compareTo(secondItem.getUpdatedAt());
         }
 
         return 0;
