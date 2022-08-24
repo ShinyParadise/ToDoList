4189953290c05e9f3e80147c69b511fec93bc28d
ShinyParadise <shinodise@gmail.com>
2022-08-24 13:40:23 +0300

    removed unused field

--- app/src/main/java/com/example/todolist/utils/SynchronousExecutorService.java
+++ app/src/main/java/com/example/todolist/utils/SynchronousExecutorService.java
@@ -6,8 +6,6 @@ import java.util.concurrent.AbstractExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class SynchronousExecutorService extends AbstractExecutorService {
-    private volatile boolean terminated;
-
     @Override
     public void shutdown() { }
 
